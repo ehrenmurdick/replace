4834d615fce5bcd00072985e11032cedeba847b3
Ehren Murdick <ehren.murdick@gmail.com>
2010-11-21 22:50:44 -0800

    green

--- lib/replace.rb
+++ lib/replace.rb
@@ -1,10 +1,18 @@
 class Replace
   def self.remove_duplicate_words(text)
+    text = text.dup
+
     word_counts = Hash.new(0)
     text.downcase.scan(/\w+/) do |match|
       word_counts[match] += 1
     end
 
-    p word_counts
+    word_counts.each do |word, count|
+      if count > 1
+        text.gsub!(/#{word}/i, '?')
+      end
+    end
+
+    text
   end
 end
--- spec/replace_spec.rb
+++ spec/replace_spec.rb
@@ -5,8 +5,10 @@ require 'replace'
 describe Replace do
   it "should replace any word which occurs more than once with ?" do
     text = <<-END
-      Foo bar baz foo qux.
+      Foo bar baz foo qux foo.
     END
-    Replace.remove_duplicate_words(text).should == "? bar baz ? qux."
+    Replace.remove_duplicate_words(text).should == <<-END
+      ? bar baz ? qux ?.
+    END
   end
 end
